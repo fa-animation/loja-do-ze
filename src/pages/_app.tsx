c061914355a9230c2f8fb8685641ccfff274ebb1
Kayo Ronald <kayoronald4321@gmail.com>
2023-05-24 12:49:51 +0000

    chore: syle import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Chakra } from '@/component/ChakraProvider/Chakra'
 import type { AppProps } from 'next/app'
 import Header from '@/layout/Header'
-import Layout from '@/layout/Layout'
+import '../styles/globals.css'
 import { AuthProvider } from '@/contexts/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
