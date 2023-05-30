faed47bd73f95dc0b3b67624170fc5e3519237e8
Kyler Kai <88721640+kyllerkai@users.noreply.github.com>
2023-05-30 13:43:44 +0000

    feat: create component AppStoreButton

--- src/component/Bannner/AppStore.tsx
+++ src/component/Bannner/AppStore.tsx
@@ -1,6 +1,7 @@
 import { Box, Heading, Text, Container, Link, Icon, Stack } from "@chakra-ui/react";
 import Typewriter from 'typewriter-effect';
 import { FaApple, FaGooglePlay } from 'react-icons/fa'
+import { AppStoreButton } from "./AppStoreButton";
 
 export const AppStores = () => {
   return (
@@ -8,7 +9,6 @@ export const AppStores = () => {
       textAlign={"center"}
       mb={8}
       w="full"
-      // p={[4, 8]}
       rounded="lg"
       pb={4}
     >
@@ -33,76 +33,14 @@ export const AppStores = () => {
           spacing={3}
           direction={{ base: 'column', sm: 'row' }}
         >
-          <Link
-            href="#"
-            w={['full', 'auto']}
-            color="white"
-            rounded="lg"
-            display="inline-flex"
-            justifyItems={"center"}
-            justifyContent="center"
-            px={4}
-            py={2.5}
-            bg="gray.700"
-            _hover={{ bg: 'gray.600' }}
-            size="lg"
-          >
-            <Icon
-              mr={3}
-              w={7}
-              h={7}
-              as={FaApple}
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="apple"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            />
-            <Box textAlign="left">
-              <Text mb={1} fontSize="xs">
-                Baixe agora na
-              </Text>
-              <Text mt={-1} fontFamily="sans-serif" fontSize="sm" fontWeight="semibold">
-                Mac App Store
-              </Text>
-            </Box>
-          </Link>
-          <Link
-            href="#"
-            w={['full', 'auto']}
-            color="white"
-            rounded="lg"
-            size="lg"
-            display="inline-flex"
-            justifyItems={"center"}
-            justifyContent="center"
-            px={4}
-            py={2.5}
-            bg="gray.700"
-            _hover={{ bg: 'gray.600' }}
-          >
-            <Icon
-              mr={3}
-              w={7}
-              h={7}
-              as={FaGooglePlay}
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="apple"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            />
-            <Box textAlign="left">
-              <Text mb={1} fontSize="xs">
-                Baixe agora no
-              </Text>
-              <Text mt={-1} fontFamily="sans-serif" fontSize="sm" fontWeight="semibold">
-                Google Play
-              </Text>
-            </Box>
-          </Link>
+          <AppStoreButton
+            icon={FaApple}
+            text="Mac App Store"
+          />
+          <AppStoreButton
+            icon={FaGooglePlay}
+            text="Google Play"
+          />
         </Stack>
       </Container>
     </Box>
