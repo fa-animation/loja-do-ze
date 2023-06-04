05e3d19385ef634af19caccc5bb366213c632c24
Kayo Ronald <kayoronald4321@gmail.com>
2023-06-04 19:45:44 +0000

    chore: teste

---
+++ src/pages/api/status/index.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  res.status(200).json({
+    vercel_region: process.env.VERCEL_REGION,
+    timezone: process.env.TZ,
+    last_commit_author: process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN,
+    last_commit_message: process.env.VERCEL_GIT_COMMIT_MESSAGE,
+    last_commit_message_sha: process.env.VERCEL_GIT_COMMIT_SHA,
+    provider: process.env.VERCEL ? 'vercel' : 'local',
+  })
+}
