58b603745cf5bf0063461b28a6c8095153725b0f
Kayo Ronald <kayoronald4321@gmail.com>
2023-06-04 13:36:05 +0000

    chore: update slide

--- src/utils/feedback.ts
+++ src/utils/feedback.ts
@@ -5,26 +5,41 @@ export const feedbackData = [
     "nome": "João Silva",
     "feedback": "A loja do Zé possui produtos de alta qualidade e um atendimento excelente. Recomendo a todos!",
     "colorBorder": "pink.500"
-  },
-  {
+  }, {
     "id": 2,
     "photo": "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "nome": "Maria Santos",
     "feedback": "Fiquei muito satisfeita com a minha compra na loja do Zé. Os preços são ótimos e a entrega foi rápida.",
     "colorBorder": "blue.500"
-  },
-  {
+  }, {
     "id": 3,
-    "photo": "https://example.com/photo3.jpg",
+    "photo": "https://engenharia360.com/wp-content/uploads/2019/05/esta-pessoa-nao-existe-engenharia360-4.png",
     "nome": "Pedro Oliveira",
     "feedback": "Sou cliente fiel da loja do Zé há anos. Sempre encontro o que preciso e a equipe é muito prestativa.",
     "colorBorder": "yellow.500"
-  },
-  {
+  }, {
     "id": 4,
-    "photo": "https://example.com/photo4.jpg",
+    "photo": "https://thumbs.dreamstime.com/b/retrato-da-jovem-mulher-com-cabelo-encaracolado-tend%C3%AAncia-115558312.jpg",
     "nome": "Ana Rodrigues",
     "feedback": "Recomendo a loja do Zé para todos os meus amigos. Produtos de qualidade e ótimo atendimento!",
     "colorBorder": "green.500"
-  }
+  }, {
+    "id": 5,
+    "photo": "https://example.com/photo5.jpg",
+    "nome": "Pedro Santos",
+    "feedback": "A loja do Zé tem uma ótima seleção de brinquedos. Comprei um carrinho de controle remoto para o meu filho e ele adorou!",
+    "colorBorder": "orange.500"
+  }, {
+    "id": 6,
+    "photo": "https://example.com/photo8.jpg",
+    "nome": "Isabela Rodrigues",
+    "feedback": "Minha filha adorou as roupas infantis da loja do Zé. São fofas, confortáveis e duráveis. Recomendei para minhas amigas!",
+    "colorBorder": "blue.500"
+  }, {
+    "id": 6,
+    "photo": "https://example.com/photo6.jpg",
+    "nome": "Mariana Silva",
+    "feedback": "A seção de lazer da loja do Zé é incrível! Comprei uma mesa de ping pong e agora me divirto muito com amigos e família.",
+    "colorBorder": "red.500"
+  },
 ]
