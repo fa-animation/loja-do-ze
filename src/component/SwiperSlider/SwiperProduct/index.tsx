f87172c11731bd9ec3f4c91a9729562450d118b7
Kayo Ronald <kayoronald4321@gmail.com>
2023-06-06 11:33:11 +0000

    chore: update slide

--- src/component/SwiperSlider/SwiperProduct/index.tsx
+++ src/component/SwiperSlider/SwiperProduct/index.tsx
@@ -1,21 +1,27 @@
 import type { NextPage } from 'next';
+import { CSSProperties } from 'react';
 import { Box } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { sliderSettings } from '../config';
 import { ProductCard } from './ProductCard';
 import { SwiperNavButtons } from '../SwiperNavButtons';
-import 'swiper/css';
+// import 'swiper/css';
+
+const slideStyles: CSSProperties = {
+  boxSizing: 'border-box',
+  maxWidth: '350px',
+};
 
 export const ProductSwiper: NextPage<any> = ({ data }) => {
   const slides = Array.from({ length: 20 }).map(
     (el, index) => `Slide ${index + 1}`
   );
   return (
-    <Box>
-      <Swiper {...sliderSettings}>
+    <Box  w="100%" h="100%">
+      <Swiper {...sliderSettings} style={{ width: '100%', height: '100%' }}>
         <SwiperNavButtons />
         {slides.map((slideContent, index) => (
-          <SwiperSlide key={index}>
+          <SwiperSlide key={index} style={slideStyles}>
             <ProductCard />
           </SwiperSlide>
         ))}
